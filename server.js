e8b16acdfe46f77ac95429864335029a9e4f1b10
IReallyDontKnow101 <76219361+IReallyDontKnow101@users.noreply.github.com>
2022-11-06 15:11:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
+let port = process.env.PORT || 3000;
 
 let initial_path = path.join(__dirname, 'public');
 
@@ -37,7 +38,7 @@ app.use((req, res) => {
     res.json("404");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(initial_path);
     console.log('Server is up!');
 });
