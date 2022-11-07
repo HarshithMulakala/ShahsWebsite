2da350e77b983680bda4a148e9ef93b64aac99ce
IReallyDontKnow101 <76219361+IReallyDontKnow101@users.noreply.github.com>
2022-11-06 18:40:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.post('/upload', (req, res) => {
 
     let imagename = date.getDate() + date.getTime() + file.name;
 
-    let path = 'public/' + imagename;
+    let path = 'public/upload/' + imagename;
 
     file.mv(path, (err, result) => {
         if (err) { throw err; }
         else {
-            res.json('/' + imagename);
+            res.json('upload/' + imagename);
         }
     });
 });
