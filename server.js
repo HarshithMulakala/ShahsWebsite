a8fe7ba684e5a337e0d6149c542eeaada58d4162
IReallyDontKnow101 <76219361+IReallyDontKnow101@users.noreply.github.com>
2022-11-23 00:43:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/upload', (req, res) => {
 
     let imagename = date.getDate() + date.getTime() + file.name;
     
-    let path = '/public/uploads/' + imagename;
+    let path = './public/uploads/' + imagename;
 
     file.mv(path, (err, result) => {
         if (err) { throw err; }
